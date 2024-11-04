9bb91c3a6adccb4105711e2eb7d5ab619c74c230
YeoSooBin <124415826+YeoSooBin@users.noreply.github.com>
2024-11-04 08:58:12 +0000

    New files baseline

---
+++ script.js
@@ -0,0 +1,9 @@
+function sayhello() {
+    console.log= "Hello!"
+}
+
+function showage() {
+    var user_input = document.getElementById(user_input).value
+
+    document.getElementById("header-age").textContent=user_input
+}
